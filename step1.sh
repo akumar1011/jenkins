4cd5903551d6cb440f06c6defd94d6399e8334ee
akumar1011 <45355058+akumar1011@users.noreply.github.com>
2019-03-12 12:18:10 +0530

    Update step1.sh
    added ssh

--- step1.sh
+++ step1.sh
@@ -1,9 +1,1 @@
-logpath="/home/akumar39/jenkins/jobs"
-logfile="step1.log"
-
-echo "----------Start: Step1----------" >> $logpath/$logfile
-date >> $logpath/$logfile
-echo "sleep for 5 second" >> $logpath/$logfile
-sleep 5
-date >> $logpath/$logfile
-echo "----------End: Step1----------" >> $logpath/$logfile
+ssh -i /c/Users/akumar39/.jenkins/sshkey/mysshkey.key akumar39@lena088.jcpenney.com /home/akumar39/jenkins/jobs/step1.sh
